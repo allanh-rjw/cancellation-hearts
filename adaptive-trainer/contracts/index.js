77772d16625b20ca32a4f1c75614e2f34430849a
allanh-rjw <58897992+allanh-rjw@users.noreply.github.com>
2026-09-05 07:12:14 -1000

    ME20 export calibration contracts

--- adaptive-trainer/contracts/index.js
+++ adaptive-trainer/contracts/index.js
@@ -1,4 +1,5 @@
 export * from './item-contracts.js';
 export * from './evidence-contracts.js';
 export * from './assessment-contracts.js';
+export * from './calibration-contracts.js';
 export * from './runtime-contracts.js';
